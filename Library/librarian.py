db5172312f9e43a50b9a3ea5e64beb16a9fab433
AbdullahSofayan <444101428@student.ksu.edu.sa>
2025-06-23 18:26:03 +0300

    Solve modules Lab

--- Library/librarian.py
+++ Library/librarian.py
@@ -1,5 +1,15 @@
 
 
+# I added this function to check if a isbn is in the library or not. 
+def isExist(library:dict, isbn:str):
+    return isbn in library.keys()
+
+
+# This function to check if a book is available or not. 
+def isAvailable(library:dict, isbn:str):
+    return library[isbn]["available"]
+
+
 
 
 def add_book(library:dict, title:str, author:str, isbn:str):
@@ -7,45 +17,60 @@ def add_book(library:dict, title:str, author:str, isbn:str):
     book = {
         'title': title,
         'author': author,
-        'isbn': isbn,
         'available': True
     }
    
-    if isbn not in library.keys():
+    if not isExist(library, isbn):
         library[isbn] = book
     else:
-        print('no')
+        print('ERROR: book already exist.')
 
     
 
 
 
-# library = {
-#     '1221': {'title': 'Oppenheimer', 'author': 'Abood', 'isbn': '1221', 'available': True},
-#     '125': {'title': 'Some Book', 'author': 'Ajmad', 'isbn': '125', 'available': True}
-# }
 
-# add_book(library, "asda", "mohammed", '1245')
-# print(library)
-
-def remove_book(library, isbn):
-    pass
+def remove_book(library:dict, isbn:str):
+    if isExist(library, isbn):
+        del library[isbn]
+    else:
+        print("ERROR: The ISBN is not exist.")
 
 
 
 
-def check_out_book(library, isbn):
-    pass
+
+def check_out_book(library:dict, isbn:str):
+    if isExist(library, isbn):
+        if isAvailable(library, isbn):
+            library[isbn]['available'] = False
+        else:
+            print(f"ERROR: The book: {library[isbn]['title']} is not Available.")
+
+    else:
+        print(f"ERROR: The ISBN: {isbn} is not exist.")
+    
+
+
+def return_book(library:dict, isbn:str):
+    if isExist(library, isbn):
+            library[isbn]['available'] = True
+
+    else:
+        print(f"ERROR: The ISBN: {isbn} is not exist.")
+
+
+def display_books(library:str):
+    keys = list(library.keys())
+     
+    for isbn in keys:
+        print(f"{library[isbn]['title']} by {library[isbn]['author']} (ISBN: {isbn}) - {'Available' if library[isbn]['available'] else 'Checked Out'}")
+    
 
 
 
 
-def return_book(library, isbn):
-    pass
 
 
-
-def display_books(library):
-    pass
 
 
